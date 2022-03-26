4ddfed95e5a5fcd07fb00fcc564e5fb20edbce90
Arty Face <artiface@has-robots-inn.com>
2022-03-26 11:31:13 -0400

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import '../css/components.css'
 function Footer() {
     return (
         <div className="Footer container">
-            <p>Artiface 2022</p>
+            <p>Artiface 2022 - Template by <a href="//github.com/heysagnik/Linkees">Sagnik</a></p>
         </div>
     )
 }
